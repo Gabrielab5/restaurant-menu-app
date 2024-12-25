720a37c455531f26f65db6036041564b3940a8e8
Gabrielab5 <87898816+Gabrielab5@users.noreply.github.com>
2024-12-25 14:06:15 +0200

    Created a Menu.vue
    component to display the menu items categorized appropriately

---
+++ public/Menu.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <h1>Menu</h1>
+      <div v-for="category in menuData" :key="category.category">
+        <h2>{{ category.category }}</h2>
+        <div v-for="item in category.items" :key="item.title">
+          <h3>{{ item.title }}</h3>
+          <p>{{ item.description }}</p>
+          <p>Price: ${{ (item.price / 100).toFixed(2) }}</p>
+          <button @click="addToCart(item)">Add to Cart</button>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['menuData'],
+    methods: {
+      addToCart(item) {
+        this.$emit('addToCart', item);
+      },
+    },
+  };
+  </script>
+  
